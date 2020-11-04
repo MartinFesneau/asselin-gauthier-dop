77555050f8b3268a565434d711b0c131424f772c
Martin Fesneau <martin.fesneau@gmail.com>
2020-11-04 15:13:10 +0100

    creat seed of 5 projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_133609) do
+ActiveRecord::Schema.define(version: 2020_11_04_140837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
-    t.string "type"
+    t.string "category"
     t.string "url"
     t.string "artist"
     t.string "name"
