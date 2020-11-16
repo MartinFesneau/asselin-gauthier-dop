b35e85f5f8cd145392a1ea37a0c1d5a0e55298a3
Martin Fesneau <martin.fesneau@gmail.com>
2020-11-16 11:50:17 +0100

    change format category, change color to variables

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
-  FORMAT = ["21by9", "16by9", "4by3", "1by1", "2,35by1"]
+  FORMAT = ["21by9", "16by9", "4by3", "1by1"]
+  CATEGORY = ["clip", "pub"]
   validates :url, presence: true  
   validates :name, presence: true  
   validates :artist, presence: true  
