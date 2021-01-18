3071e91772779e6f60e61334580227e6a039990b
Martin Fesneau <martin.fesneau@gmail.com>
2021-01-18 09:40:03 +0100

    url method to model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -24,7 +24,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.friendly.find(params[:id])
-    @project.url = urlify(@project.url)
+    @project.url = Project.urlify(@project.url)
   end
 
   def edit
@@ -59,38 +59,5 @@ class ProjectsController < ApplicationController
       :category, :format , :url, :artist, :name, 
       :director, :producer, photos: []
     )
-  end
-
-  def regexpify(url)
-    # delete the channel part of the youtube url if necessary, maybe possible to do it in the regexp
-    if url.include?"&"
-      index = url.index("&")
-      url.slice!(index..-1)
-    end
-    # regexp to indetify the source (youtube or vimeo) and the video id
-    regexp = /(\A.{11}.(?<source>[a-z]*).*=(?<youtube_id>.*)|\A.{5}:\/\/.{9}\/(?<vimeo_id>.*))/
-    match_data = url.strip.match(regexp)
-  end
-  
-  def extract_source(url)
-    match_data = regexpify(url)
-    source = match_data[:source]
-  end
-  
-  def extract_id(url)
-    match_data = regexpify(url)
-    source = extract_source(url)
-    source == "youtube" ? video_id = match_data[:youtube_id] : video_id = match_data[:vimeo_id]
-  end
-  
-  def urlify(url)
-    # create the good embed url for the video
-    source = extract_source(url)
-    video_id = extract_id(url)
-    if source == "youtube"
-      url = "https://www.youtube.com/embed/#{video_id}"
-    else
-      url = "https://player.vimeo.com/video/#{video_id}"
-    end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -12,5 +12,40 @@ class Project < ApplicationRecord
 
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  class << Project
+    def regexpify(url)
+      # delete the channel part of the youtube url if necessary, maybe possible to do it in the regexp
+      if url.include?"&"
+        index = url.index("&")
+        url.slice!(index..-1)
+      end
+      # regexp to identify the source (youtube or vimeo) and the video id
+      regexp = /(\A.{11}.(?<source>[a-z]*).*=(?<youtube_id>.*)|\A.{5}:\/\/.{9}\/(?<vimeo_id>.*))/
+      match_data = url.strip.match(regexp)
+    end
+    
+    def extract_source(url)
+      match_data = regexpify(url)
+      source = match_data[:source]
+    end
+    
+    def extract_id(url)
+      match_data = regexpify(url)
+      source = extract_source(url)
+      source == "youtube" ? video_id = match_data[:youtube_id] : video_id = match_data[:vimeo_id]
+    end
+    
+    def urlify(url)
+      # create the good embed url for the video
+      source = extract_source(url)
+      video_id = extract_id(url)
+      if source == "youtube"
+        url = "https://www.youtube.com/embed/#{video_id}"
+      else
+        url = "https://player.vimeo.com/video/#{video_id}"
+      end
+    end
+  end
 end
 
