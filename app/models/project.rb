b7e622e726bacb238361f0e8e10c6e3b707142ec
Martin Fesneau <martin.fesneau@gmail.com>
2021-02-12 17:36:48 +0100

    styling

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,8 +4,6 @@ class Project < ApplicationRecord
   validates :url, presence: true  
   validates :name, presence: true  
   validates :artist, presence: true  
-  validates :director, presence: true  
-  validates :producer, presence: true  
   validates :format, presence: true
   validates :category, presence: true
   has_many_attached :photos
